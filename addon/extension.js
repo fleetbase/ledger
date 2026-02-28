abac1b11086723b5068a9c2fda0a6c6c238663d5
Ronald A. Richardson <me@ron.dev>
2026-02-28 10:42:19 +0800

    New extension structure

---
+++ addon/extension.js
@@ -0,0 +1,9 @@
+import { Widget, ExtensionComponent, Hook } from '@fleetbase/ember-core/contracts';
+
+export default {
+    setupExtension(app, universe) {
+        // const menuService = universe.getService('universe/menu-service');
+        // const hookService = universe.getService('universe/hook-service');
+        // const widgetService = universe.getService('universe/widget-service');
+    }
+};
