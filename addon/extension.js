3b8d3b0997ddcf3ebbe529b89369740cdb461acc
Ronald A. Richardson <me@ron.dev>
2026-03-01 14:29:46 +0800

    fix engine.js and extension.js

--- addon/extension.js
+++ addon/extension.js
@@ -2,8 +2,11 @@ import { Widget, ExtensionComponent, Hook } from '@fleetbase/ember-core/contract
 
 export default {
     setupExtension(app, universe) {
-        // const menuService = universe.getService('universe/menu-service');
-        // const hookService = universe.getService('universe/hook-service');
-        // const widgetService = universe.getService('universe/widget-service');
+        const menuService = universe.getService('universe/menu-service');
+        const hookService = universe.getService('universe/hook-service');
+        const widgetService = universe.getService('universe/widget-service');
+
+        // Register header navigation
+        menuService.registerHeaderMenuItem('Ledger', 'console.ledger', { icon: 'calculator', priority: 1 });
     }
 };
