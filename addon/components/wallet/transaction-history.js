e7a7cbd35b856638d4d9b4b7e25b52423ea7597b
Fleetbase Dev <dev@fleetbase.io>
2026-03-02 21:48:01 -0500

    refactor: use Table component in wallet transaction-history; remove p-4 wrapper; add balance-after translation

--- addon/components/wallet/transaction-history.js
+++ addon/components/wallet/transaction-history.js
@@ -5,9 +5,60 @@ import { task } from 'ember-concurrency';
 
 export default class WalletTransactionHistoryComponent extends Component {
     @service fetch;
+    @service intl;
+
     @tracked transactions = [];
     @tracked meta = null;
     @tracked page = 1;
+
+    get columns() {
+        return [
+            {
+                label: this.intl.t('column.date'),
+                valuePath: 'createdAt',
+                resizable: true,
+                sortable: false,
+                width: '130px',
+            },
+            {
+                label: this.intl.t('column.type'),
+                valuePath: 'type',
+                resizable: true,
+                sortable: false,
+                width: '120px',
+            },
+            {
+                label: this.intl.t('column.description'),
+                valuePath: 'description',
+                resizable: true,
+                sortable: false,
+            },
+            {
+                label: this.intl.t('column.amount'),
+                valuePath: 'amount',
+                cellComponent: 'table/cell/currency',
+                resizable: true,
+                sortable: false,
+                width: '120px',
+            },
+            {
+                label: this.intl.t('column.balance-after'),
+                valuePath: 'balance_after',
+                cellComponent: 'table/cell/currency',
+                resizable: true,
+                sortable: false,
+                width: '130px',
+            },
+            {
+                label: this.intl.t('column.status'),
+                valuePath: 'status',
+                cellComponent: 'table/cell/status',
+                resizable: true,
+                sortable: false,
+                width: '110px',
+            },
+        ];
+    }
 
     constructor(owner, args) {
         super(owner, args);
@@ -16,7 +67,9 @@ export default class WalletTransactionHistoryComponent extends Component {
 
     @task *loadTransactions() {
         const wallet = this.args.wallet;
-        if (!wallet?.id) return;
+        if (!wallet?.id) {
+            return;
+        }
         try {
             const result = yield this.fetch.get(
                 `wallets/${wallet.id}/transactions`,
